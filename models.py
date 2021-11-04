fedb1d14608451b696f080ff1e6085cb7c83bbca
aditya635 <55912563+aditya635@users.noreply.github.com>
2021-11-04 20:47:58 +0530

    models done

--- models.py
+++ models.py
@@ -20,9 +20,11 @@ class Posts(Base):
     id = Column(Integer, primary_key=True, index=True)
     caption = Column(String)
     url = Column(URLType)
+    upvote = Column(Integer, default=0)
     user_id = Column(Integer, ForeignKey('users.id'))
     creator = relationship("User", back_populates="posts")
     comments = relationship("Comments",back_populates="post")
+    
 
 class Comments(Base):
     __tablename__ = "comments"
@@ -33,6 +35,14 @@ class Comments(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
     post = relationship("Posts",back_populates="comments")
     creator = relationship("User",back_populates="comments")
+
+class UpVote(Base):
+    __tablename__ ="upvotepost"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'),primary_key=True)
+    post_id = Column(Integer, ForeignKey('posts.id'),primary_key=True)
+
 
 
 
