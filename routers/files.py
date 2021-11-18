6442d8ed2a8946083b9e889424e246fc8c94925a
aditya635 <55912563+aditya635@users.noreply.github.com>
2021-11-18 19:00:35 +0530

    worked on subpost

--- routers/files.py
+++ routers/files.py
@@ -14,13 +14,18 @@ from schemas import Caption
 router = APIRouter(prefix='/files')
 
 @router.post('/',response_model=schemas.File,tags=['file'])
-def upload(cap:str = Form(...) , file: UploadFile = File(...) ,db: Session = Depends(get_db), get_current_user: schemas.TokenData = Depends (oauth2.get_current_user)):
+def upload( sub:int = Form(...) , cap:str = Form(...) , file: UploadFile = File(...) ,db: Session = Depends(get_db), get_current_user: schemas.TokenData = Depends (oauth2.get_current_user)):
     result = cloudinary.uploader.upload(file.file)
     url = result.get("url")
     new_post = models.Posts(caption=cap, url= url , user_id = get_current_user.id)
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
+    ids = db.query(models.Posts).filter(models.Posts.id == new_post.id).first()
+    new_d = models.SubredditPost(sub_id = sub, post_id = ids)
+    db.add(new_d)
+    db.commit()
+    db.refresh(new_d)
     return new_post
 
 @router.get('/',response_model= List[schemas.File],tags=['file'])
